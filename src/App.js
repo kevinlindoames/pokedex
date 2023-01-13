695650b1fa29751884096aab2e2adb3e04927800
Kevin Lindo Ames <82423639+kevinlindoames@users.noreply.github.com>
2023-01-12 23:12:01 -0500

    pokedex

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import SearchBar from './Components/Searchbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Navbar/>
+      <SearchBar/>
+      <div className='App'>
+        
+      </div>
+    </div>  
   );
 }
 
---
+++ src/Components/Searchbar.js
@@ -0,0 +1,12 @@
+import React from  "react";
+
+const SearchBar =() => {
+    return (
+        <div className="searchbar-container">
+            <div className="searchbar">
+                <input placeholder="Buscar Pokemon"></input>
+            </div>
+        </div>
+    )
+}
+export default SearchBar;
